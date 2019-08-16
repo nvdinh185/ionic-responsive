9be1762b5e0c585207267794a8e989e30884b610
nvdinh185 <nvdinh0905671805@gmail.com>
2019-08-16 08:30:32 +0700

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,5 +6,5 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor() {}
 }
--- www/build/main.js
+++ www/build/main.js
@@ -1,6 +1,6 @@
 webpackJsonp([0],{
 
-/***/ 108:
+/***/ 107:
 /***/ (function(module, exports) {
 
 function webpackEmptyAsyncContext(req) {
@@ -13,7 +13,7 @@ function webpackEmptyAsyncContext(req) {
 webpackEmptyAsyncContext.keys = function() { return []; };
 webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
 module.exports = webpackEmptyAsyncContext;
-webpackEmptyAsyncContext.id = 108;
+webpackEmptyAsyncContext.id = 107;
 
 /***/ }),
 
@@ -38,9 +38,8 @@ webpackEmptyAsyncContext.id = 149;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomePage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GridPage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(54);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -51,21 +50,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-
-var HomePage = /** @class */ (function () {
-    function HomePage(navCtrl) {
-        this.navCtrl = navCtrl;
+var GridPage = /** @class */ (function () {
+    function GridPage() {
     }
-    HomePage = __decorate([
+    GridPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\pages\home\home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <ion-row id="header">\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      header cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      header cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      header cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      header cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      header cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      header cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row id="footer">\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      footer cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      footer cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      footer cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      footer cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      footer cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      footer cot 6\n    </ion-col>\n  </ion-row>\n</ion-content>'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\pages\home\home.html"*/
+            selector: 'page-grid',template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\pages\grid\grid.html"*/'<ion-header>\n\n  <ion-navbar>\n    <ion-title>grid</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n    <ion-grid>\n        <ion-row>\n          <ion-col col-xl-4 col-lg-4 col-md-3 col-sm-2>\n            <div>\n              1 of 3\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-4 col-md-3 col-sm-2>\n            <div>\n              2 of 3 (wider)\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-1 col-md-3 col-sm-2>\n            <div>\n              3 of 3\n            </div>\n          </ion-col>\n        </ion-row>\n        <ion-row>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1>\n            <div>\n              1 of 3\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1>\n            <div>\n              2 of 3 (wider)\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1>\n            <div>\n              3 of 3\n            </div>\n          </ion-col>\n        </ion-row>\n      </ion-grid>\n</ion-content>\n'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\pages\grid\grid.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
-    ], HomePage);
-    return HomePage;
+        __metadata("design:paramtypes", [])
+    ], GridPage);
+    return GridPage;
 }());
 
-//# sourceMappingURL=home.js.map
+//# sourceMappingURL=grid.js.map
 
 /***/ }),
 
@@ -90,12 +87,12 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(30);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(54);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(108);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(258);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(266);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(267);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(191);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_grid_grid__ = __webpack_require__(268);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(268);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__pages_grid_grid__ = __webpack_require__(191);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -152,7 +149,7 @@ var AppModule = /** @class */ (function () {
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MyApp; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__pages_grid_grid__ = __webpack_require__(268);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__pages_grid_grid__ = __webpack_require__(191);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -169,7 +166,7 @@ var MyApp = /** @class */ (function () {
         this.rootPage = __WEBPACK_IMPORTED_MODULE_1__pages_grid_grid__["a" /* GridPage */];
     }
     MyApp = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\app\app.html"*/
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\app\app.html"*/'<ion-nav [root]="rootPage"></ion-nav>\n'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\app\app.html"*/
         }),
         __metadata("design:paramtypes", [])
     ], MyApp);
@@ -184,7 +181,7 @@ var MyApp = /** @class */ (function () {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return GridPage; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomePage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -196,19 +193,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-var GridPage = /** @class */ (function () {
-    function GridPage() {
+var HomePage = /** @class */ (function () {
+    function HomePage() {
     }
-    GridPage = __decorate([
+    HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-grid',template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\pages\grid\grid.html"*/'<ion-header>\n\n  <ion-navbar>\n    <ion-title>grid</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n    <ion-grid>\n        <ion-row>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              1 of 3\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              2 of 3 (wider)\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              3 of 3\n            </div>\n          </ion-col>\n        </ion-row>\n        <ion-row>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              1 of 3\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              2 of 3 (wider)\n            </div>\n          </ion-col>\n          <ion-col col-xl-4 col-lg-3 col-md-2 col-sm-1 col-xs-1>\n            <div>\n              3 of 3\n            </div>\n          </ion-col>\n        </ion-row>\n      </ion-grid>\n</ion-content>\n'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-table\src\pages\grid\grid.html"*/,
+            selector: 'page-home',template:/*ion-inline-start:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\pages\home\home.html"*/'<ion-header>\n  <ion-navbar>\n    <ion-title>Home</ion-title>\n  </ion-navbar>\n</ion-header>\n\n<ion-content padding>\n  <ion-row id="header">\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      header cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      header cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      header cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      header cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      header cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      header cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      d1 cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      d1 cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      d1 cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      d1 cot 6\n    </ion-col>\n  </ion-row>\n  <ion-row id="footer">\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      footer cot 1\n    </ion-col>\n    <ion-col class="hidden-sm" col-xl-2 col-sm-3>\n      footer cot 2\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      footer cot 3\n    </ion-col>\n    <ion-col col-xl-2 col-sm-2>\n      footer cot 4\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1>\n      footer cot 5\n    </ion-col>\n    <ion-col col-xl-2 col-sm-1 class="hidden-xl">\n      footer cot 6\n    </ion-col>\n  </ion-row>\n</ion-content>'/*ion-inline-end:"D:\DINHNV\MyData\LapTrinhDiDong\IONIC_Baitap\ionic-responsive\src\pages\home\home.html"*/
         }),
         __metadata("design:paramtypes", [])
-    ], GridPage);
-    return GridPage;
+    ], HomePage);
+    return HomePage;
 }());
 
-//# sourceMappingURL=grid.js.map
+//# sourceMappingURL=home.js.map
 
 /***/ })
 
